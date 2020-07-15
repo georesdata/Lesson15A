0641579e2e2a92c488944ffa4b922e4ad55ffeea
georesdata <grdobbs@gmail.com>
2020-07-15 18:06:35 -0400

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Hello world. Wear your mask!"/>
   </div>
 </template>
 
